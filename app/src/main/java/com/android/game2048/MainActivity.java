dcf925fb58342816572537a55951cc295a8f7837
moujunqiang <705174710@qq.com>
2020-11-05 16:08:18 +0800

    123

--- app/src/main/java/com/android/game2048/MainActivity.java
+++ app/src/main/java/com/android/game2048/MainActivity.java
@@ -12,13 +12,10 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.android.game2048.utils.SPData;
 import love.smartalk.game2048.widget.Love2048Layout;
 
-public class MainActivity extends AppCompatActivity implements Love2048Layout.OnLove2048Listener {
+public class MainActivity extends AppCompatActivity {
 
-    TextView tvScore;
-    TextView tvBestScore;
     Love2048Layout llGameView;
 
 
@@ -31,8 +28,6 @@ public class MainActivity extends AppCompatActivity implements Love2048Layout.On
     }
 
     private void initView() {
-        tvScore = findViewById(R.id.tv_score);
-        tvBestScore = findViewById(R.id.tv_best_score);
         llGameView = findViewById(R.id.ll_game_view);
         findViewById(R.id.tv_restart).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,44 +36,7 @@ public class MainActivity extends AppCompatActivity implements Love2048Layout.On
             }
         });
 
-        llGameView.setLove2048Listener(this);
-        tvBestScore.setText("最高分: " + SPData.getBestScore());
     }
 
-    @Override
-    public void onScoreChanged(int score) {
-        if (SPData.getBestScore() < score) {
-            tvBestScore.setText("最高分: " + score);
-            SPData.saveBestScore(score);
-        }
-
-        String scoreStr = "当前分数: " + score;
-        tvScore.setText(scoreStr);
-    }
-
-    @Override
-    public void onGameOver() {
-        showGameOver();
-    }
-
-    private void showGameOver() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("2048");
-        builder.setMessage("游戏结束是否重新开始? ");
-        builder.setPositiveButton("重新开始", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                llGameView.restart();
-                dialog.dismiss();
-            }
-        });
-        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-            }
-        });
-        builder.create().show();
-    }
 
 }
