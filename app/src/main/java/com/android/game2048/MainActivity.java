84e8e13863d5433b25ff320ba9b815edcda9da94
moujunqiang <705174710@qq.com>
2020-11-17 09:10:44 +0800

    123

--- app/src/main/java/com/android/game2048/MainActivity.java
+++ app/src/main/java/com/android/game2048/MainActivity.java
@@ -1,42 +1,118 @@
 package com.android.game2048;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
-
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import love.smartalk.game2048.widget.Love2048Layout;
 
 public class MainActivity extends AppCompatActivity {
 
-    Love2048Layout llGameView;
+    public static MainActivity mainActivity = null;
+    private TextView Score;
+    public static int score = 0;//当前得分
+    private TextView maxScore;
+    private ImageView share;
+    private Button restart;
+    private GameView gameView;
 
+    public MainActivity() {
+        mainActivity = this;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initView();
-    }
-
-    private void initView() {
-        llGameView = findViewById(R.id.ll_game_view);
-        findViewById(R.id.tv_restart).setOnClickListener(new View.OnClickListener() {
+        Score = (TextView) findViewById(R.id.Score);
+        maxScore = (TextView) findViewById(R.id.maxScore);
+        maxScore.setText(getSharedPreferences("pMaxScore", MODE_PRIVATE).getInt("maxScore", 0) + "");
+        share = (ImageView) findViewById(R.id.share);
+        share.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                llGameView.restart();
+                Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                shareIntent.setType("text/plain");
+                String s = "我在“2048”游戏中的得分为" + maxScore.getText() + "，你敢来挑战吗？点击进入>>http://www.amazon.cn/dp/B01KV5AK2Q";
+                shareIntent.putExtra(Intent.EXTRA_TEXT, s);
+                startActivity(Intent.createChooser(shareIntent, "炫耀一下"));
+            }
+        });
+        gameView = (GameView)findViewById(R.id.gameView);
+        restart = (Button) findViewById(R.id.restart);
+        restart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                GameView.startGame();
             }
         });
 
+
     }
 
+    public static MainActivity getMainActivity() {
+        return mainActivity;
+    }
+
+    //分数清零
+    public void clearScore() {
+        score = 0;
+        showScore();
+    }
+
+    //分数增加
+    public void addScore(int i) {
+
+        score += i;
+        showScore();
+        SharedPreferences pref = getSharedPreferences("pMaxScore", MODE_PRIVATE);
+
+        //若当前得分超出最高记录，则更新之
+        if (score > pref.getInt("maxScore", 0)) {
+            SharedPreferences.Editor editor = pref.edit();
+            editor.putInt("maxScore", score);
+            editor.commit();
+            maxScore.setText(pref.getInt("maxScore", 0) + "");
+        }
+
+    }
+
+    //显示当前得分
+    public void showScore() {
+        Score.setText(score + "");
+    }
+
+    @Override
+    public void onBackPressed() {
+        createExitTipDialog();
+    }
+
+    private void createExitTipDialog() {
+        new AlertDialog.Builder(MainActivity.this)
+                .setMessage("确认退出吗？")
+                .setTitle("提示")
+                .setPositiveButton("确认", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                        finish();
+                    }
+                })
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                })
+                .show();
+    }
 
 }
+
